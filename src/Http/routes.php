12ddb5d46883951cab86b39606e18acb0ebf917f
Jerbi Youssef <y.jerbi@armonya.fr>
2019-11-05 15:37:30 +0100

    add route banOrUnban user

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -8,6 +8,7 @@ Route::group(
     function () {
         Route::get('/', 'VisitLogController@index')->name('__visitlog__');
         Route::delete('delete_visitlog/{id}', 'VisitLogController@destroy')->name('__delete_visitlog__');
+        Route::post('ban_user_ip/{id}', 'VisitLogController@banOrUnbanUserIp')->name('__ban_or_unban_user_ip__');
         Route::delete('delete_visitlog_all', 'VisitLogController@destroyAll')->name('__delete_visitlog_all__');
     }
 );
